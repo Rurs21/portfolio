3317c784e528044ebcf98dad6c2901ff236153ef
Rurs21 <rurs01@proton.me>
2023-12-06 12:37:24 -0500

    format & clean

--- src/js/flower.js
+++ src/js/flower.js
@@ -8,20 +8,20 @@
  *
  * Components:
  * - r(θ): Radial distance from the origin to a point on the curve for a given angle θ.
- * 
+ *
  * - θ: The polar angle, measured in radians. As θ varies, it causes the curve to be traced out.
  *       Starts from 0 and increases up to θ_max to form the full flower shape.
- * 
+ *
  * - θ_max: The maximum value for θ. It determines the extent to which the rose is drawn. 
  *          For a complete flower shape, θ_max might be set to a multiple of 2π, but it can be 
  *          adjusted to generate partial flowers or specific curve spans.
  * 
  * - a, b: Constants that determine the initial size (a) and spacing (b) of the spirals.
- * 
+ *
  * - e^-kθ: Introduces an exponential decay based on θ. 'e' is the base of natural logarithm.
  *          'k' determines the decay rate; larger 'k' leads to faster spiral decay.
- * 
- * - c sin(θ_max/n θ): A sinusoidal component that gives the petal-like oscillations. 
+ *
+ * - c sin(θ_max/n θ): A sinusoidal component that gives the petal-like oscillations.
  *          'c' defines the amplitude or size of the petals.
  *          'n' dictates the number of petals or oscillations.
  *          θ_max/n is a normalization factor for consistent petal count across θ's range.
@@ -34,7 +34,7 @@
 export function archimedeanFlower(a, b, c, n, k, thetaIncrement, thetaMax) {
 	let points = [];
 	let maxPetals = n;
-	
+
 	for (let theta = 0; theta < thetaMax; theta += thetaIncrement) {
 		let dynamicN = (maxPetals / thetaMax) * theta;  // Gradually increase the number of petals
 
@@ -60,7 +60,7 @@ export function archimedeanFlower(a, b, c, n, k, thetaIncrement, thetaMax) {
 
 		// Append the closest point found to the list
 		points.push(points[closestPointIndex]);
-	}  
+	}
 
 	return points;
 }
