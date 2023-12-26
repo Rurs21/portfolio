1f6a50c870cfcda56815099a4f8b2ba2429981fc
Rurs21 <rurs01@proton.me>
2023-12-26 16:38:25 -0500

    change router & setup navigation
    + add onRemove util function
    + active link
    + fix greeting on route change

--- src/js/utils/misc.js
+++ src/js/utils/misc.js
@@ -8,7 +8,41 @@
  * @param {Document} doc - The document in which to check for loaded CSS stylesheets.
  * @returns {boolean} Returns `true` if there are CSS stylesheets loaded in the specified document; otherwise, returns `false`.
  */
-export function isCssLoaded(doc) {
+function isCssLoaded(doc) {
 	const styleSheets = doc.styleSheets
-	return (styleSheets.length > 0)
+	return styleSheets.length > 0
 }
+
+/**
+ * Attach a callback function to be executed when the specified element is removed from the DOM.
+ *
+ * This function uses a MutationObserver to detect when the element is removed from its parent node.
+ * When the removal is detected, the provided callback function is executed, and the observer is disconnected.
+ *
+ * @param {Node} element - The DOM element to watch for removal.
+ * @param {function} callback - The callback function to execute when the element is removed.
+ * @throws {Error} Throws an error if the provided element is not attached to the DOM.
+ * @returns {MutationObserver} Returns a MutationObserver instance for further interaction if needed.
+ */
+function onRemove(element, callback) {
+	const parent = element.parentNode
+	if (!parent) {
+		throw new Error("The node must be attached to the DOM")
+	}
+
+	const observer = new MutationObserver((mutations) => {
+		for (const mutation of mutations) {
+			for (const removedNode of mutation.removedNodes) {
+				if (removedNode === element) {
+					observer.disconnect()
+					callback()
+				}
+			}
+		}
+	})
+
+	observer.observe(parent, { childList: true })
+	return observer
+}
+
+export { isCssLoaded, onRemove }
