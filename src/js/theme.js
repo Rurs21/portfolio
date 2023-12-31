cf2657bd99e9499561535ccf6c34a0e19000c7c6
Rurs21 <rurs01@proton.me>
2023-12-31 19:18:10 -0400

    use app class to manage state
    and other stuff.. yikes!

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,16 +1,28 @@
 const body = document.body
 
-const schemes = ['light','dark','system']
+const schemes = ["light", "dark", "system"]
 
+// TODO remove class lol
 class Scheme {
+	constructor() {
+		this.value = this.getUserScheme()
+	}
 
-	constructor(schemeToggler, icons) {
-		this.toggler = schemeToggler
-		this.icons = icons
+	get value() {
+		return this.getUserScheme()
+	}
 
-		this.toggleScheme()
-		// Add a click event listener to the scheme-toggle button
-		this.toggler.addEventListener("click", () => {this.toggleScheme()})
+	set value(scheme) {
+		if (!schemes.includes(scheme)) {
+			throw new Error(`Scheme '${scheme}' is not a valid scheme`)
+		}
+
+		body.classList.remove("light", "dark")
+		if (scheme != "system") {
+			body.classList.add(scheme)
+		}
+		// Save the scheme preference to localStorage
+		localStorage.setItem("scheme", scheme)
 	}
 
 	getUserScheme() {
@@ -19,37 +31,15 @@ class Scheme {
 	}
 
 	toggleScheme() {
-		var scheme = this.getUserScheme()
+		var scheme = this.value
 		// cycle through next schemes
 		var idx = schemes.indexOf(scheme)
 		idx++
-		if (idx >= (schemes.length - 1)) {
-			idx = 0;
+		if (idx >= schemes.length - 1) {
+			idx = 0
 		}
-		this.changeScheme(schemes[idx])
-	}
-
-	changeScheme(scheme) {
-		if (!schemes.includes(scheme)) {
-			throw new Error(`Scheme '${scheme}' is not a valid scheme`)
-		}
-
-		if (scheme == "dark" || scheme == "light") {
-			body.classList.add(scheme)
-			if (scheme == "light") {
-				body.classList.remove("dark")
-				this.toggler.replaceChildren(this.icons.light)
-
-			} else if (scheme == "dark") {
-				body.classList.remove("light")
-				this.toggler.replaceChildren(this.icons.dark)
-			}
-		} else {
-			body.classList.remove("light","dark")
-			this.toggler.replaceChildren(this.icons.system)
-		}
-		// Save the scheme preference to localStorage
-		localStorage.setItem("scheme", scheme)
+		this.value = schemes[idx]
+		return this.value
 	}
 }
 
