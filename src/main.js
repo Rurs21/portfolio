53f0eab08cae694ebe6e4f364d461958b79ce9a8
Rurs21 <rurs01@proton.me>
2024-01-07 14:07:59 -0500

    Cache every route
    + add locationchanged event

--- src/main.js
+++ src/main.js
@@ -7,36 +7,44 @@ import { main } from "./webgl/"
 import initialize from "./initialize.js"
 import { isCssLoaded } from "./utils/misc.js"
 
-var app
+import page404 from "/404/index.html"
+import pageWebgl from "/webgl/index.html"
+
+var app = undefined
 
 document.addEventListener("DOMContentLoaded", init)
+window.addEventListener("load", initComponents)
 
 function init(event) {
 	app = new App()
 	app.router = initRouter()
+}
 
-	if (isCssLoaded(event.target)) {
-		initialize(app)
-			.visual()
-			.menu()
-			.navigation()
-			.icons().then((init) => {
-				init.controls.language()
-				init.controls.scheme()
-			})
-	} else {
-		initialize(app)
-			.nonCssFeatures()
-			.navigation()
-			.controls.language()
+function initComponents(event) {
+	try {
+		if (isCssLoaded(document)) {
+			initialize(app)
+				.visual()
+				.menu()
+				.navigation()
+				.controls.language()
+				.controls.scheme()
+		} else {
+			initialize(app)
+				.nonCssFeatures()
+				.navigation()
+				.controls.language()
+		}
+	} catch (error) {
+		console.error(`Error while initiliazing app \n${error}`)
 	}
 }
 
 function initRouter() {
 	const router = new Router()
-	router.addRoute("/", greeting).cacheDocument(document)
-	router.addRoute("/webgl", main)
-
+	router.addRoute("/", greeting, document)
+	router.addRoute(404, undefined, page404)
+	router.addRoute("/webgl", main, pageWebgl)
 	router.resolveRoute()
 	return router
 }
