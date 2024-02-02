bcbef9b13b5fb58ea9120caaf18dcbc1d20a71c1
Rurs21 <rurs21@protonmail.com>
2024-02-01 22:28:22 -0500

    slim-down html & shaders files

--- plugins/slim.js
+++ plugins/slim.js
@@ -1,17 +1,18 @@
 import path from "path"
 import fs from "fs"
 import fsp from "fs/promises"
+import { minify } from "html-minifier"
 import { optimize } from "svgo"
-import svgoConfig from "./svgo.config.js"
+import { svgoConfig, htmlMinifyConfig } from "./configs.js"
 
 /**
  * Plugins that replace the img with referenced svg with the actual svg inline inside html files
  */
-export default function htmlSvgInline(options = {}) {
+export default function slim(options = {}) {
 	const postfix = "?html-import"
 
 	return {
-		name: "html-svg-inline",
+		name: "slim",
 		enforce: "pre",
 
 		async transformIndexHtml(html, ctx) {
@@ -36,16 +37,37 @@ export default function htmlSvgInline(options = {}) {
 		},
 
 		async load(id) {
-			if (!id.endsWith(postfix)) {
-				return
+			async function readFile(id) {
+				let content = await fsp.readFile(id, "utf-8")
+				return content
+					.replace("\t", "") // tab
+					.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, "") // comment
+					.replace(/(\r\n|\n|\r)/gm, "") // newline
 			}
 
-			id = cleanUrl(id)
-			let fileLocation = path.dirname(id)
-			let htmlContent = await fsp.readFile(id, "utf-8")
-			htmlContent = replaceImgWithInlineSVG(htmlContent, fileLocation)
+			if (id.endsWith(".html")) {
+				let htmlContent = await readFile(id)
+				htmlContent = minify(htmlContent, htmlMinifyConfig)
+				return htmlContent
+			}
 
-			return `export default ${JSON.stringify(htmlContent)}`
+			if (id.endsWith(postfix)) {
+				id = cleanUrl(id)
+				let content = await readFile(id)
+				let fileLocation = path.dirname(id)
+				content = replaceImgWithInlineSVG(content, fileLocation)
+				content = minify(content, htmlMinifyConfig)
+				return `export default ${JSON.stringify(content)}`
+			}
+
+			if (id.endsWith("?raw")) {
+				id = cleanUrl(id)
+				// GLSL files
+				if (id.endsWith(".vert") || id.endsWith(".frag")) {
+					let content = await readFile(id)
+					return `export default ${JSON.stringify(content)}`
+				}
+			}
 		}
 	}
 }
@@ -67,21 +89,17 @@ function replaceImgWithInlineSVG(html, fileLocation) {
 		try {
 			// Read the SVG file content
 			let svgContent = retrieveSVG(src, fileLocation)
-
 			// Capture common attributes
 			const capturedAttributes = captureAttriubtes(attributes)
-
 			// Add <title> and <description> with "aria-labelledby"
 			if (capturedAttributes["alt"]) {
 				svgContent = setAriaAttributes(svgContent, capturedAttributes)
 			}
-
 			// Append the attributes to <svg>
 			svgContent = svgContent.replace(
 				/<svg([^>]*)>/,
 				`<svg$1${capturedAttributes.toString()}>`
 			)
-
 			// return optimize svg
 			return optimize(svgContent, svgoConfig).data
 		} catch (error) {
@@ -89,15 +107,21 @@ function replaceImgWithInlineSVG(html, fileLocation) {
 			return match // return the original <image>
 		}
 	}
-
 	html = html.replace(imgRegex, replacer)
-
 	return html
 }
 
 function captureAttriubtes(attributes) {
 	const obj = {}
-	obj.toString = propertiesToString
+	obj.toString = function() {
+		let str = ""
+		for (var key in this) {
+			if (typeof obj[key] == "string") {
+				str += ` ${key}="${obj[key]}" `
+			}
+		}
+		return str
+	}
 
 	const attrRegex =
 		/(class|id|style|height|width|alt|aria-\w+|data-\w+)="([^"]*)"/g
@@ -107,16 +131,6 @@ function captureAttriubtes(attributes) {
 	}
 
 	return obj
-
-	function propertiesToString() {
-		let str = ""
-		for (var key in this) {
-			if (typeof obj[key] == "string") {
-				str += ` ${key}="${obj[key]}" `
-			}
-		}
-		return str
-	}
 }
 
 function setAriaAttributes(svgString, capturedAttributes) {
@@ -129,10 +143,8 @@ function setAriaAttributes(svgString, capturedAttributes) {
 	}
 	const alt = capturedAttributes["alt"]
 	const id = capturedAttributes["id"]
-
 	const titleId = id + "-title"
 	const descId = id + "-desc"
-
 	svgString = svgString.replace(
 		/<svg([^>]*)>/,
 		`<svg$1><title id="${titleId}">${alt}</title><desc id="${descId}">${alt}</desc>`
@@ -157,6 +169,5 @@ function retrieveSVG(src, from) {
 		const dataURIsvg = src.slice(src.indexOf(",") + 1)
 		svgString = decodeURIComponent(dataURIsvg)
 	}
-
 	return svgString
 }
