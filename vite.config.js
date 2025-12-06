0eb864804a9b1f35ef5afbf4e911ac51780da91b
Rurs21 <rurs21@protonmail.com>
2025-12-06 14:47:17 -0500

    add service worker
    + use dynamic imports for views (chunks)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import path from "path"
 import slim from "./plugins/slim"
+import pkg from './package.json' with { type: 'json' }
 
 export default {
 	root: "src",
@@ -14,8 +15,26 @@ export default {
 		}
 	},
 	build: {
+		target: 'baseline-widely-available',
+		manifest: true,
 		assetsInlineLimit: 12288,
+		outDir: path.resolve(__dirname, "dist"),
 		emptyOutDir: true,
-		outDir: path.resolve(__dirname, "dist")
+		rollupOptions: {
+			input: {
+				index: 'src/index.html', // Your main entry point
+				sw: 'src/sw.js',    // Your service worker entry point
+			},
+			output: {
+				entryFileNames: (chunkInfo) => {
+					if (chunkInfo.name === 'sw')
+						return 'sw.js'
+					else
+						return `assets/[name]-v${pkg.version}.js`
+				},
+				chunkFileNames: "assets/[name]-[hash:6].js",
+				assetFileNames: `assets/[name]-v${pkg.version}.[ext]`
+			},
+		},
 	}
 }
